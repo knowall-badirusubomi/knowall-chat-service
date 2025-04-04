96d19c0ee104179aac2e3ef4c1b49a4fd4ab98fd
Olasubomi O. Badiru <badirusubomi@icloud.com>
2025-04-04 08:50:46 -0600

    feat: jwt verfied auth middleware

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
-# Knowall Chat Service 
-Frameworks utilised: Flask, Flask.socketio
+# Knowall Chat Service
 
+Frameworks utilised: Flask, Flask.socketio
 
 ## Install dependencies
 
-> pip install -r requirements.txt
+- Highly recommend you execute in a virtual environment
+  > pip install -r requirements.txt
 
 ## Provide environment variables
+
 > cp .env-sample.txt .env
 
-
 ## Initiate server
+
 > python src/server.py
+
+### Dependencies
+
+- Redis server [link](https://redis.io/docs/latest/develop/clients/redis-py/)
--- src/server.py
+++ src/server.py
@@ -8,6 +8,8 @@ from werkzeug.wrappers import Request, Response, ResponseStream
 import jwt
 from dotenv import load_dotenv
 
+
+
 load_dotenv()  # take environment variables
 JWT_SECRET_TOKEN = os.getenv('JWT_SECRET_TOKEN')
 # Initialize Redis client
@@ -53,7 +55,7 @@ class AuthMiddleware:
 Chats = {}
 app = ChatFlaskApp(__name__)
 # cache  = Cache(app=app)
-socketio = SocketIO(app,debug=True,cors_allowed_origins='*',async_mode='threading')
+socketio = SocketIO(app,debug=True,cors_allowed_origins='*',async_mode='gevent')
 # Initialize Flask-Caching with Redis
 
 
@@ -77,6 +79,18 @@ def get_chats():
 
     #   return jsonify({'message': 'Chats retrieved successfully'})
 
+
+
+def validateToken(token):
+    try:
+        decoded = jwt.decode(token, JWT_SECRET_TOKEN, algorithms=["HS256"])
+        return True
+    except Exception as e:
+        print('Invalid Token')
+        return False
+
+
+
 @app.route('/chats/:sessionId', methods=['POST'])
 def add_item():
       # Get the item name from the request body
@@ -90,16 +104,29 @@ def add_item():
 
       return jsonify({'message': 'Item added successfully'})
 
+
+def authmiddleware(fn):
+    def middleware(*args):
+        if validateToken(request.authorization.token):
+            fn(*args)
+        else:
+            fn(False)
+    
+    return middleware
+
+
 @socketio.on("chat")
+@authmiddleware
 def handleChat(data):
-    agentSession = redis_client.hgetall(f"agent-session:{request.sid}")
-    if not agentSession:
+    # agentSession = redis_client.hgetall(f"agent-session:{request.sid}")
+    if type(data) == bool and data == False:
         print("Agent not authenticated, disconnecting.")
         socketio.emit("unauthorized", room=request.sid)
         return disconnect()
-    
+    else:
+        response = f"Server response to: {data['message']}"
     # Some logic here
-    emit("chatResponse", data, broadcast=True)
+    emit("chatResponse", {'response': response}, room=request.sid)
 
 @socketio.on("connect")
 def onConnect():
@@ -130,10 +157,12 @@ def authenticate():
 
 
 if __name__ =='__main__':
-    # cache.init_app(app)
-
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
     
+    port = 8081
     #Start Server
-    port = '8081'
-    app.run('127.0.0.1', port, debug=True)
+    server = pywsgi.WSGIServer(('0.0.0.0', port), app, handler_class=WebSocketHandler)
+    server.serve_forever()
+    # app.run('127.0.0.1', port, debug=True)
     print(f"Knowall chat service is live on port: {port}")
